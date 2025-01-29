93deaa109d6ab822ff4e490d981f3ea587d0d59c
Hamza Farra <h.farra@fida.de>
2025-01-29 15:23:37 +0100

    Hide update message

--- src/lib/components/layout/UpdateInfoToast.svelte
+++ src/lib/components/layout/UpdateInfoToast.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
 	import { getContext, createEventDispatcher } from 'svelte';
 
 	const dispatch = createEventDispatcher();
@@ -11,9 +11,9 @@
 		current: WEBUI_VERSION,
 		latest: WEBUI_VERSION
 	};
-</script>
+</script> -->
 
-<div
+<!-- <div
 	class="flex items-start bg-[#F1F8FE] dark:bg-[#020C1D] border border-[3371D5] dark:border-[#03113B] text-[#3371D5] dark:text-[#6795EC] rounded-lg px-3.5 py-3 text-xs max-w-80 pr-2 w-full shadow-lg"
 >
 	<div class="flex-1 font-medium">
@@ -36,4 +36,4 @@
 			<XMark />
 		</button>
 	</div>
-</div>
+</div> -->
